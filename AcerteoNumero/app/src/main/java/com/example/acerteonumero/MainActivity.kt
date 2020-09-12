e820f42b262e94d3d1fe8a82baa67211fc82b3f4
Romero Caetano <romero.caetano@digivox.com.br>
2020-09-11 23:35:29 -0300

    Acerte o Numero

---
+++ AcerteoNumero/app/src/main/java/com/example/acerteonumero/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.acerteonumero
+
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvDica1 : TextView
+    private lateinit var tvDica2 : TextView
+    private lateinit var tvDica3 : TextView
+    private lateinit var edTentativa : EditText
+    private lateinit var btTentativa : Button
+    private lateinit var tvPreResultText: TextView
+    private lateinit var tvResultNumber: TextView
+    private lateinit var tvResultText: TextView
+    private lateinit var tvSorting: TextView
+    private var isResumed: Boolean = false;
+    private var randomValue: Int = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.tvDica1 = findViewById(R.id.tvDica1)
+        this.tvDica2 = findViewById(R.id.tvDica2)
+        this.tvDica3 = findViewById(R.id.tvDica3)
+        this.edTentativa = findViewById(R.id.edTentativa)
+        this.btTentativa = findViewById(R.id.btTentativa)
+        this.tvPreResultText = findViewById(R.id.tvPreResultText)
+        this.tvResultText = findViewById(R.id.tvResultText)
+        this.tvResultNumber = findViewById(R.id.tvResultNumber)
+        this.tvSorting = findViewById(R.id.tvSorting)
+        this.sortGame()
+        btTentativa.setOnClickListener{
+            val tentativa = this.edTentativa.text
+                this@MainActivity.tvPreResultText.setText("o Número Sorteado é:")
+                this@MainActivity.tvResultNumber.setText(this.randomValue.toString())
+            if(tentativa.toString().toIntOrNull() === this.randomValue){
+                this@MainActivity.tvResultText.setText("Você Acertou!")
+            }else{
+                this@MainActivity.tvResultText.setText("Você Errou!")
+            }
+            for(x in 1..5){
+                Handler().postDelayed(Runnable {
+                    this@MainActivity.tvSorting.setText("Sorteando Novamente"+".".repeat(x))
+                }, (1000*x).toLong())
+
+            }
+            Handler().postDelayed(Runnable {
+                sortGame();
+            }, 5000)
+        }
+    }
+
+
+    override fun onResume(){
+        super.onResume();
+        if(isResumed){
+            this.sortGame();
+        }
+
+    }
+    override fun onPause(){
+        super.onPause()
+        this.isResumed = true;
+    }
+    fun sortGame() {
+        this.randomValue = Random.nextInt(1, 101)
+        Log.i("APP_ACERTE", randomValue.toString())
+        val divisibleNumbers = ArrayList<Int>();
+        val isEven = randomValue % 2 == 0;
+        var divisibleNumbersCount = 0;
+        for(x in 2..10){
+            if(randomValue % x == 0){
+                divisibleNumbers.add(x)
+            }
+        }
+        for(x in 2 until randomValue){
+            if(randomValue % x == 0){
+                divisibleNumbersCount += 1
+            }
+        }
+        this@MainActivity.tvDica1.setText("Entre 1 e 10, é divisivel por: "+divisibleNumbers.joinToString(" "))
+        this@MainActivity.tvDica2.setText(if(isEven) "O Número é Par" else "O Número não é par")
+        this@MainActivity.tvDica3.setText("Tem "+divisibleNumbersCount+" divisores")
+        clearFields();
+    }
+    fun clearFields(){
+        this@MainActivity.edTentativa.setText("")
+        this@MainActivity.tvResultText.setText("")
+        this@MainActivity.tvResultNumber.setText("")
+        this@MainActivity.tvPreResultText.setText("")
+        this@MainActivity.tvSorting.setText("")
+    }
+}
